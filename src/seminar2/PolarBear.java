9105f95d930f8ab075c7272f12b1dd7de9356811
br798 <61415550+br798@users.noreply.github.com>
2020-02-24 14:35:17 +0200

    Second commit

--- src/seminar2/PolarBear.java
+++ src/seminar2/PolarBear.java
@@ -1,5 +1,20 @@
 package seminar2;
 
-public class PolarBear {
+public abstract class PolarBear extends Bear{
+	public static final int POLAR_BEAR_SPEED=15;
+	
+	public PolarBear() {
+		
+	}
 
+	@Override
+	public String toString() {
+		return "PolarBear []";
+	}
+
+	public static int getPolarBearSpeed() {
+		return POLAR_BEAR_SPEED;
+	}
+	
+	
 }
